9fa78ab37aa5007d7cf81e4bc4b7322251d8b4ad
Jonathan Beliën <dev@jbelien.be>
2021-04-24 16:51:15 +0200

    Update config.php
    Add German to languages in the process.
    Related to https://github.com/EqualStreetNames/equalstreetnames/issues/173#issuecomment-826074502

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ return [
     'relationId' => 62638,
 
     // Languages in which you want to extract Wiki informations
-    'languages' => ['en'],
+    'languages' => ['de', 'en'],
 
     // Exclude datas from beeing extracted. Usefull to exclude streets at the edge of the city for example
     'exclude' => [
